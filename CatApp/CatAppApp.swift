7cc07c8826e9811fe2ebe57094085e747c123ea3
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-23 16:40:46 +0530

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by Isuru Ariyarathna on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatAppTests/CatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppTests.swift
+//  CatAppTests
+//
+//  Created by Isuru Ariyarathna on 2024-10-23.
+//
+
+import Testing
+@testable import CatApp
+
+struct CatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
